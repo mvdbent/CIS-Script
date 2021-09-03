4d4b59788ec87aef6b6e1490787ec7150d0776ba
MvdBent <mvdbent@gmail.com>
2021-09-03 15:30:41 +0200

    added arguments

--- Utils/Assemble.sh
+++ Utils/Assemble.sh
@@ -3,38 +3,98 @@
 script_dir=$(dirname ${0:A})
 projectfolder=$(dirname $script_dir)
 
-# destination
-endResult=${projectfolder}/Build/CISBenchmarkRemediationReport.sh
+help () {
+  echo "usage: assemble.sh [-s] --separate"
+  echo 
+  echo "builds separate CIS BEnchmark script from the fragements."
+  exit
+}
 
-# add shebang
-echo "#!/bin/zsh" > ${endResult}
-echo >> ${endResult}
+buildScript () {
+    # destination
+    endResult=${projectfolder}/Build/CISBenchmarkRemediationReport.sh
 
-# add version and date
-version=$(cat "${projectfolder}/Fragments/Version.sh")
-versiondate=$(date +%F) 
-echo "VERSION=\"$version\"" >> ${endResult}
-echo "VERSIONDATE=\"$versiondate\"" >> ${endResult}
-echo >> ${endResult}
-
-# add header
-cat ${projectfolder}/Fragments/Header.sh >> ${endResult}
-
-# loop over fragments
-for filePath in ${projectfolder}/Fragments/OrgScores/OrgScore*.sh; do
-
-    # fragment name
-    fileName=$(basename ${filePath})
-    echo "Add ${fileName} to script"
-
-    # add script
-    tail -n +7 ${filePath} >> ${endResult}
+    # add shebang
+    echo "#!/bin/zsh" > ${endResult}
     echo >> ${endResult}
 
-done
+    # add version and date
+    version=$(cat "${projectfolder}/Fragments/Version.sh")
+    versiondate=$(date +%F) 
+    echo "VERSION=\"$version\"" >> ${endResult}
+    echo "VERSIONDATE=\"$versiondate\"" >> ${endResult}
+    echo >> ${endResult}
 
-# add footer
-cat ${projectfolder}/Fragments/Footer.sh >> ${endResult}
+    # add header
+    cat ${projectfolder}/Fragments/Header.sh >> ${endResult}
 
-# make script executable
-chmod +x ${endResult}
+    # loop over fragments
+    for filePath in ${projectfolder}/Fragments/OrgScores/OrgScore*.sh; do
+
+        # fragment name
+        fileName=$(basename ${filePath})
+        echo "Add ${fileName} to script"
+
+        # add script
+        tail -n +7 ${filePath} >> ${endResult}
+        echo >> ${endResult}
+
+    done
+
+    # add footer
+    cat ${projectfolder}/Fragments/Footer.sh >> ${endResult}
+
+    # make script executable
+    chmod +x ${endResult}
+}
+
+# build seperate Scripts
+buildSeperateScript () {
+    # loop over fragments
+    for filePath in ${projectfolder}/Fragments/OrgScores/OrgScore*.sh; do
+
+        # fragment name
+        fileName=$(basename ${filePath})
+
+        # destination
+        endResult="${projectfolder}/Build/Scripts/${fileName}"
+
+        # add shebang
+        echo "#!/bin/zsh" > ${endResult}
+        echo >> ${endResult}
+
+        # add version and date
+        version=$(cat "${projectfolder}/Fragments/Version.sh")
+        versiondate=$(date +%F)
+        echo "VERSION=\"$version\"" >> ${endResult}
+        echo "VERSIONDATE=\"$versiondate\"" >> ${endResult}
+        echo >> ${endResult}
+
+        # add header
+        cat ${projectfolder}/Fragments/Header.sh >> ${endResult}
+        
+        # add script
+        tail -n +7 ${filePath} >> ${endResult}
+        echo >> ${endResult}
+
+        # add footer
+        cat ${projectfolder}/Fragments/Footer.sh >> ${endResult}
+        
+        # make script executable
+        chmod +x ${endResult}
+
+        echo "${fileName} created"
+    done
+}
+
+case $1 in 
+    -s | --separate)
+       buildSeperateScript
+    ;;
+    -h | --help)
+        help
+    ;;
+    *)
+        buildScript
+    ;;
+esac
