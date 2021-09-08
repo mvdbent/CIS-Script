d0bf21d90097e296b990d734d092d759f652fd34
mvdbent <mvdbent@gmail.com>
2021-09-08 17:09:01 +0200

    added remediation

--- Fragments/OrgScores/OrgScore5_11.sh
+++ Fragments/OrgScores/OrgScore5_11.sh
@@ -16,13 +16,27 @@ if [[ "${auditResult}" == "1" ]]; then
 	method="Script"
 	remediate="Script > sudo security authorizationdb read system.preferences > /tmp/system.preferences.plist && sudo defaults write /tmp/system.preferences.plist shared -bool false && sudo security authorizationdb write system.preferences < /tmp/system.preferences.plist"
 
-	adminSysPrefs="$(security authorizationdb read system.preferences 2> /dev/null | grep -A 1 "<key>shared</key>" | grep -c "<false/>")"
+	adminSysPrefs="$(security authorizationdb read system.preferences /dev/null 2>&1 | grep -A 1 "<key>shared</key>" | grep -c "<false/>")"
 	if [[ "${adminSysPrefs}" == "1" ]]; then
 		result="Passed"
 		comment="Require an administrator password to access system-wide preferences: Enabled"
 	else 
 		result="Failed"
 		comment="Require an administrator password to access system-wide preferences: Disabled"
+		# Remediation
+		if [[ "${remediateResult}" == "enabled" ]]; then
+		security authorizationdb read system.preferences > /tmp/system.preferences.plist 2>&1
+		defaults write /tmp/system.preferences.plist shared -bool false 2>&1
+		security authorizationdb write system.preferences < /tmp/system.preferences.plist 2>&1
+		# re-check
+		adminSysPrefs="$(security authorizationdb read system.preferences /dev/null 2>&1 | grep -A 1 "<key>shared</key>" | grep -c "<false/>")"
+			if [[ "${adminSysPrefs}" == "1" ]]; then
+				result="Passed After Remediation"
+				comment="Require an administrator password to access system-wide preferences: Enabled"
+			else
+				result="Failed After Remediation"
+			fi
+		fi
 	fi
 fi
 printReport
