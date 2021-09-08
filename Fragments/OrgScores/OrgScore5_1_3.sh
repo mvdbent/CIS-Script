3b2e78475d599e07b4600b2c4b508e3be67f6a8b
mvdbent <mvdbent@gmail.com>
2021-09-08 14:09:00 +0200

    added remediation

--- Fragments/OrgScores/OrgScore5_1_3.sh
+++ Fragments/OrgScores/OrgScore5_1_3.sh
@@ -23,6 +23,22 @@ if [[ "${auditResult}" == "1" ]]; then
 	else 
 		result="Failed"
 		comment="Check ${sysPermissions} System folder for world writable files"
+		# Remediation
+		if [[ "${remediateResult}" == "enabled" ]]; then
+			IFS=$'\n'
+			for sysPermissions in $( /usr/bin/find /System/Volumes/Data/System -type d -perm -2 -ls | /usr/bin/grep -v "Public/Drop Box" ); do
+			/bin/chmod -R o-w "$sysPermissions"
+			done
+			unset IFS
+
+			sysPermissions="$(find /System/Volumes/Data/System -type d -perm -2 -ls 2>&1 | grep -v "Public/Drop Box" | wc -l | xargs)"
+			if [[ "${sysPermissions}" == "0" ]]; then
+				result="Passed After Remediation"
+				comment="All System folder for world are not writable files"
+			else
+				result="Failed After Remediation"
+			fi
+		fi	
 	fi
 fi
 printReport
