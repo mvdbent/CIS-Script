0c2d83ce2af5644d440ed1152c508ca0a634d067
mvdbent <mvdbent@gmail.com>
2021-09-08 17:25:40 +0200

    added remediation

--- Fragments/OrgScores/OrgScore5_14.sh
+++ Fragments/OrgScores/OrgScore5_14.sh
@@ -16,13 +16,29 @@ if [[ "${auditResult}" == "1" ]]; then
 	method="Script"
 	remediate="https://support.apple.com/en-us/HT202277"
 
-	policyBanner="$(find /Library/Security -name 'PolicyBanner.rtf*' | wc -l)"
+	policyBanner="$(find /Library/Security -name 'PolicyBanner.*' | wc -l | xargs)"
 	if [[ "${policyBanner}" == "1" ]]; then
 		result="Passed"
 		comment="Login window banner: Enabled"
 	else 
 		result="Failed"
 		comment="Login window banner: Disabled"
+		# Remediation
+		if [[ "${remediateResult}" == "enabled" ]]; then
+		PolicyBannerText="CIS Example Login Window banner"
+		/bin/echo "$PolicyBannerText" > "/Library/Security/PolicyBanner.txt"
+		/bin/chmod 755 "/Library/Security/PolicyBanner."* 
+		# re-check
+		policyBanner="$(find /Library/Security -name 'PolicyBanner.*' | wc -l | xargs)"
+		if [[ "${policyBanner}" == "1" ]]; then
+				result="Passed After Remediation"
+				comment="Login window banner: Enabled"
+			else
+				result="Failed After Remediation"
+			fi
+		fi
+
+		
 	fi
 fi
 printReport
