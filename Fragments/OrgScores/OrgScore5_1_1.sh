6c7643ba76840038781fdbe35018805568de10b8
mvdbent <mvdbent@gmail.com>
2021-09-08 13:51:52 +0200

    added remediation

--- Fragments/OrgScores/OrgScore5_1_1.sh
+++ Fragments/OrgScores/OrgScore5_1_1.sh
@@ -23,6 +23,24 @@ if [[ "${auditResult}" == "1" ]]; then
 	else 
 		result="Failed"
 		comment="Home Folders: Insecure"
+		# Remediation
+		if [[ "${remediateResult}" == "enabled" ]]; then
+			IFS=$'\n'
+			for userDirs in $( /usr/bin/find /System/Volumes/Data/Users -mindepth 1 -maxdepth 1 -type d -perm -1 | /usr/bin/grep -v "Shared" | /usr/bin/grep -v "Guest" ); do
+			/bin/chmod og-rwx "$userDirs"
+			done
+			unset IFS
+
+			homeFolders="$(find /Users -mindepth 1 -maxdepth 1 -type d -perm -1 2>&1 | grep -v "Shared" | grep -v "Guest" | wc -l | xargs)"
+			if [[ "${homeFolders}" == "0" ]]; then
+				result="Passed After Remediation"
+				comment="Home Folders: Secure"
+			else
+				result="Failed After Remediation"
+			fi
+		fi
+
+		
 	fi
 fi
 printReport
