0d35221fcb94ebe4d03c3c8be5212c8bca49cca8
mvdbent <mvdbent@gmail.com>
2021-09-23 00:49:25 +0200

    diabled open location

--- Fragments/Footer.sh
+++ Fragments/Footer.sh
@@ -6,7 +6,7 @@
 echo >> "${CISBenchmarkReport}"
 echo "Security report - $(date -u)" >> "${CISBenchmarkReport}"
 
-open "${CISBenchmarkReportPath}"
+# open "${CISBenchmarkReportPath}"
 # open -a Numbers "${CISBenchmarkReport}"
 # open -a "Microsoft Excel" "${CISBenchmarkReport}"
 
